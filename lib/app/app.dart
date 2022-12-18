a6cd2f8a359971b7fb3b13c5db5f438d06341fbb
mohammed-xp <m7md20039579@gmail.com>
2022-12-18 11:08:14 +0200

    initial commit

---
+++ lib/app/app.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class MyApp extends StatelessWidget {
+  // default constructor
+  // const MyApp({Key? key}) : super(key: key);
+
+
+  // named constructor
+  MyApp._internal();
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
