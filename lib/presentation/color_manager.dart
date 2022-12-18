97cd45f65d4e1d5a880c14bbca02a8c1b1bc7f0c
mohammed-xp <m7md20039579@gmail.com>
2022-12-18 14:04:54 +0200

    Createing Colors Manager

---
+++ lib/presentation/color_manager.dart
@@ -0,0 +1,9 @@
+import 'dart:ui';
+
+class ColorManager {
+  
+  static const Color primary = Color(0xffED9728);
+  static const Color darkGrey = Color(0xff525252);
+  static const Color grey = Color(0xff737477);
+  static const Color lightGrey = Color(0xff9E9E9E);
+}
