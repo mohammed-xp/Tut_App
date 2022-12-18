c77d7e45e9617077cf89665dec1ba4164ec7bc75
mohammed-xp <m7md20039579@gmail.com>
2022-12-18 13:34:47 +0200

    Createing Aplication class

--- lib/app/app.dart
+++ lib/app/app.dart
@@ -1,12 +1,13 @@
 import 'package:flutter/material.dart';
 
 class MyApp extends StatelessWidget {
-  // default constructor
-  // const MyApp({Key? key}) : super(key: key);
-
 
   // named constructor
   MyApp._internal();
+
+  static final MyApp _instance = MyApp._internal(); // singleton or single instance
+
+  factory MyApp() => _instance; // factory
 
   @override
   Widget build(BuildContext context) {
