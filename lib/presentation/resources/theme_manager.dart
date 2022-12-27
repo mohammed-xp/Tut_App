db1016632ec85ca27aa34e91f4d91a9af177ee4b
mohammed-xp <m7md20039579@gmail.com>
2022-12-27 17:36:54 +0200

    Start adding routes manager

--- lib/presentation/resources/theme_manager.dart
+++ lib/presentation/resources/theme_manager.dart
@@ -1,8 +1,9 @@
 import 'package:flutter/material.dart';
-import 'package:tut_app/presentation/color_manager.dart';
-import 'package:tut_app/presentation/font_manager.dart';
-import 'package:tut_app/presentation/styles_manager.dart';
-import 'package:tut_app/presentation/value_manager.dart';
+import 'package:tut_app/presentation/resources/color_manager.dart';
+import 'package:tut_app/presentation/resources/font_manager.dart';
+import 'package:tut_app/presentation/resources/styles_manager.dart';
+
+import 'value_manager.dart';
 
 ThemeData getApplicationTheme() {
   return ThemeData(
