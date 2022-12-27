c20b49a1496a6ce3f64cc73f8fd44a2b1fc6b71e
mohammed-xp <m7md20039579@gmail.com>
2022-12-27 16:08:01 +0200

    Adding Card Theme

--- lib/presentation/theme_manager.dart
+++ lib/presentation/theme_manager.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:tut_app/presentation/color_manager.dart';
+import 'package:tut_app/presentation/value_manager.dart';
 
 ThemeData getApplicationTheme() {
   return ThemeData(
@@ -10,7 +11,11 @@ ThemeData getApplicationTheme() {
     disabledColor: ColorManager.grey1,
 
     // cardView theme
-
+    cardTheme: CardTheme(
+      color: ColorManager.white,
+      shadowColor: ColorManager.grey,
+      elevation: AppSize.s8,
+    )
 
     // app bar theme
 
