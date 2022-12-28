0ac1ef772a788eab0554e4b8728a434cc96e0d8b
mohammed-xp <m7md20039579@gmail.com>
2022-12-28 13:33:06 +0200

    Adding onBoarding images in assets + Adding onBoarding title and subTitle in strings_manager.dart

--- lib/presentation/resources/assets_manager.dart
+++ lib/presentation/resources/assets_manager.dart
@@ -1,5 +1,10 @@
-const String IMAGE_PATH = "assets/images";
+const String imagePath = "assets/images";
 
 class ImageAssets{
-  static const String splashLogo = "$IMAGE_PATH/splash_logo.png";
+  static const String splashLogo = "$imagePath/splash_logo.png";
+  static const String onBoardingImage_1 = "$imagePath/onboarding_image1.svg";
+  static const String onBoardingImage_2 = "$imagePath/onboarding_image2.svg";
+  static const String onBoardingImage_3 = "$imagePath/onboarding_image3.svg";
+  static const String onBoardingImage_4 = "$imagePath/onboarding_image4.svg";
+
 }
